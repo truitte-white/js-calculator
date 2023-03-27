b3b61e9277df1a1c3edc48da315d0b84a38deb95
Truitte White <truitte.white@rfsmart.com>
2023-03-27 10:53:28 -0400

    second changes

--- script.js
+++ script.js
@@ -6,41 +6,44 @@ let resetButton = document.querySelector('.reset-button')
 
 //this looks at the number button that is pushed, grabs its value and displays it on the calculator screen
 numButton.forEach(function(numBtn) {
-    numBtn.addEventListener('click', function(press){
+    numBtn.addEventListener('click', function(press) {
         let value = press.target.innerText 
         calcDisplay.innerText += value 
     })
 })
 
 //this looks at the operator button that is pushed, grabs its value and displays it on the calculator screen
+//have been trying to figure a way to throw an error if any operator buttons other than - or + are pushed first
 operationButton.forEach(function(opBtn) {
-    opBtn.addEventListener('click', function(press){
+    opBtn.addEventListener('click', function(press) {
         let value = press.target.innerText 
-        if(press.target.innerText  == `sqrt`){
-            calcDisplay.innerText = Math.sqrt(calcDisplay.innerText).toFixed(9);
+        if (press.target.innerText  == `sqrt`) {
+            calcDisplay.innerText = Math.sqrt(calcDisplay.innerText).toFixed(9)
         }
-        else if(press.target.id  == 'sqr'){
-            calcDisplay.innerText = eval(calcDisplay.innerText) *eval(calcDisplay.innerText);
+        else if (press.target.innerText  == 'sqrd') {
+            calcDisplay.innerText = (calcDisplay.innerText * calcDisplay.innerText)
         }
-        else if(press.target.innerText  == 'tan'){
-            calcDisplay.innerText = Math.tan(calcDisplay.innerText).toFixed(9);
+        else if (press.target.innerText  == 'tan') {
+            calcDisplay.innerText = Math.tan(calcDisplay.innerText).toFixed(9)
         }
-        else if(press.target.innerText  == '%'){
-            calcDisplay.innerText = (calcDisplay.innerText/100);
+        else if (press.target.innerText  == '%') {
+            calcDisplay.innerText = (calcDisplay.innerText/100)
         }
         else {
-        calcDisplay.innerText += value 
+        calcDisplay.innerText = value 
         }
     })
 })
 
-//this looks at the equals button when it is pushed, evaluates the buttons pushed first, them tries to evaluate the answer, if an error is thrown in the console then a message is displayed
+//this looks at the equals button when it is pushed, evaluates the buttons pushed first, them tries to evaluate the answer 
+//if an error is thrown in the console then a message is displayed
 equalsButton.addEventListener('click', function() {
     try {
     let answer = eval(calcDisplay.innerText)
         if (answer.toString().length >= 10) {
             calcDisplay.innerText = answer.toExponential(7)
-        } else {
+        } 
+        else {
             calcDisplay.innerText = answer
         } 
     } catch (error) {  
